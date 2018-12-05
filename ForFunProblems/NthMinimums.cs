daf5f0abf03a0061696c7a5d0377fd50aa216784
esoemad5 <esoemad5@gmail.com>
2018-12-05 14:55:36 -0600

    This problem is a lot more complex than I thought. Wrote Insert method; wrote MakeHead_Left_RightNull to re-organize a left-weighted BST so the head and head.left nodes have no right sub-trees.

--- ForFunProblems/NthMinimums.cs
+++ ForFunProblems/NthMinimums.cs
@@ -115,6 +115,7 @@ namespace InterviewProblems
 
         private class BST
         {
+            private Node head = null;
             private class Node
             {
                 private int data;
@@ -135,12 +136,12 @@ namespace InterviewProblems
                 
             }
 
-            private Node head = null;
+            
 
-            public BST (int[] input)
+            public BST (int[] input) // builds a tree, nothing special
             {
                 Console.WriteLine("Building tree");
-                Node current;
+               
                 foreach(int element in input)
                 {
                     if(head == null)
@@ -149,85 +150,105 @@ namespace InterviewProblems
                         continue;
                     }
 
-                    current = head;
-                    while (true)
-                    {
-                        if(element <= current.Data)
-                        {
-                            if(current.Left == null)
-                            {
-                                current.Left = new Node(element);
-                                break;
-                            }
-                            else
-                            {
-                                current = current.Left;
-                            }
-                        }
-                        else // element > current.Data
-                        {
-                            if (current.Right == null)
-                            {
-                                current.Right = new Node(element);
-                                break;
-                            }
-                            else
-                            {
-                                current = current.Right;
-                            }
-                        }
-                    }
+                    Insert(element);
+                    
                 }
                 Console.WriteLine();
             }
 
+            public void Insert(int input)
+            {
+                Node current = head;
+                while (true)
+                {
+                    if (input <= current.Data)
+                    {
+                        if (current.Left == null)
+                        {
+                            current.Left = new Node(input);
+                            break;
+                        }
+                        else
+                        {
+                            current = current.Left;
+                        }
+                    }
+                    else // input[i] > current.Data
+                    {
+                        if (current.Right == null)
+                        {
+                            current.Right = new Node(input);
+                            break;
+                        }
+                        else
+                        {
+                            current = current.Right;
+                        }
+                    }
+                }
+            }
+
+            private void MakeHead_Left_RightNull()
+            {
+                if (head.Left.Right != null)
+                {
+                    //remake the head.Left subtree with the right-most value of it as the head(of the subtree, not the entire tree)
+                    //Get the largest node and it's parent
+                    Node rightParent = head.Left;
+                    Node rightmostChild = head.Left.Right;
+                    while (rightmostChild.Right != null)
+                    {
+                        rightParent = rightmostChild;
+                        rightmostChild = rightmostChild.Right;
+                    }
+
+                    //Get the largest node's left-most child
+                    Node leftmostChildOfLargestNode = rightmostChild.Left;
+                    while(leftmostChildOfLargestNode.Left != null)
+                    {
+                        leftmostChildOfLargestNode = leftmostChildOfLargestNode.Left;
+                    }
+
+                    //We have all the references we need. Time to move the sub-sub-tree
+                    rightParent.Right = null;
+                    leftmostChildOfLargestNode.Left = head.Left;
+                    head.Left = rightmostChild;
+                }
+            }
 
             public BST(int[] input, int n) //for nth min. will only construct a partial tree
             {
-                Console.WriteLine("Building tree");
-                Node current;
-                foreach (int element in input)
-                {
-                    if (head == null)
-                    {
-                        head = new Node(element);
-                        continue;
-                    }
+                if(n > input.Length) { throw new Exception("n is larger than the array!"); }
+                head = new Node(input[0]);
 
-                    current = head;
-                    int leftCount = 0;
-                    int rightCount = 0;
-                    while (true)
+                // Create a left-weighted BST, thus the head is the nth largest in the subset.
+                for (int i = 1; i < n; i++)
+                {
+                    if (input[i] > head.Data)
                     {
-                        if (element <= current.Data)
-                        {
-                            if (current.Left == null)
-                            {
-                                current.Left = new Node(element);
-                                break;
-                            }
-                            else
-                            {
-                                current = current.Left;
-                            }
-                        }
-                        else // element > current.Data
-                        {
-                            if (current.Right == null)
-                            {
-                                current.Right = new Node(element);
-                                break;
-                            }
-                            else
-                            {
-                                current = current.Right;
-                            }
-                        }
+                        Node newHead = new Node(input[i]);
+                        newHead.Left = head;
+                        head = newHead;
+                    }
+                    else
+                    {
+                        Insert(input[i]);
+                    }
+                    
+                }
+                
+                // Make sure head.Left.Right = null
+                
+
+                for(int i = n; i < input.Length; i++)
+                {
+                    if(input[i] < head.Data)
+                    {
+
                     }
                 }
-                Console.WriteLine();
             }
-            private void PrintTree(Node start)
+            private void PrintTree(Node start) // in-order traversal
             {
                 
                 if(start.Left != null)
@@ -243,7 +264,7 @@ namespace InterviewProblems
 
             public void PrintTree()
             {
-                Console.WriteLine("Printing tree (data/left/right)");
+                Console.WriteLine("Printing in-order traversal");
                 PrintTree(head);
                 Console.WriteLine();
             }
@@ -253,7 +274,7 @@ namespace InterviewProblems
         {
             int output = 0;
 
-            BST tree = new BST(input);
+            BST tree = new BST(input, n);
             tree.PrintTree();
 
             return output;
